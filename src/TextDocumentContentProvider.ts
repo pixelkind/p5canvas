0a0747c12cf98e495ac65d06ee10cb6bc96c7d5c
pixelkind <github@pixelkind.com>
2017-10-12 17:37:54 +0200

    Updating p5 and loading html preview from external template file

--- src/TextDocumentContentProvider.ts
+++ src/TextDocumentContentProvider.ts
@@ -1,6 +1,7 @@
 'use strict';
 
 import * as vscode from 'vscode';
+import * as fs from 'fs';
 
 export class TextDocumentContentProvider implements vscode.TextDocumentContentProvider {
     
@@ -32,15 +33,30 @@ export class TextDocumentContentProvider implements vscode.TextDocumentContentPr
         let editor = vscode.window.activeTextEditor;
         let text = editor.document.getText();
         let path = vscode.extensions.getExtension("garrit.p5canvas").extensionPath;
+
+        let elements = [];
+
+        let htmlhead = fs.readFileSync(path + '/assets/html_head.html', 'UTF-8');
+        elements.push(htmlhead);
+        let htmlbody = fs.readFileSync(path + '/assets/html_body.html', 'UTF-8');
         
-        let begin: string = "<!DOCTYPE html><html><head><script src=\"" + path + "/assets/p5.min.js\"></script><script src=\"https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.5.11/addons/p5.sound.min.js\"></script>"
-        let jquery: string = "<script src=\"" + path + "/assets/jquery-3.2.1.min.js\"></script>";
-        let websocketSetup: string = "<script src=\"" + path + "/assets/websocketlog.js\"></script><script>setupWebsocket('" + this.server + "');</script>";
-        let p5resize: string = "<script src=\"" + path + "/assets/p5setup.js\"></script>";
-        let errorHandler: string = "<script src=\"" + path + "/assets/errorHandler.js\"></script>";
-        let js = "<script id=\"code\">" + text + "</script>";
-        let end: string = "<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\"><style> body {padding: 0; margin: 0;} </style></head><body></body></html>"
-        return begin + jquery + websocketSetup + p5resize + errorHandler + js + end;
+        let p5lib: string = '<script src="' + path + '/assets/p5.min.js"></script>';
+        elements.push(p5lib);
+        let p5sound: string = '<script src="' + path + '/p5.sound.min.js"></script>';
+        elements.push(p5sound);
+        let jquery: string = '<script src="' + path + '/assets/jquery-3.2.1.min.js"></script>';
+        elements.push(jquery);
+        let websocketSetup: string = '<script src="' + path + '/assets/websocketlog.js"></script><script>setupWebsocket("' + this.server + '");</script>';
+        elements.push(websocketSetup);
+        let p5resize: string = '<script src="' + path + '/assets/p5setup.js"></script>';
+        elements.push(p5resize);
+        let errorHandler: string = '<script src="' + path + '/assets/errorHandler.js"></script>';
+        elements.push(errorHandler);
+        let js = '<script id="code">' + text + '</script>';
+        elements.push(js);
+        elements.push(htmlbody);
+
+        return elements.join('');
     }
 
     private errorHtml(error: string): string {
