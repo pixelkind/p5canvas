f9fe8e445ea3f21f418a310707441762abd0d30f
Garrit Schaap <contact@garritschaap.com>
2019-04-04 00:17:07 +0200

    Update formatting and vscode engine

--- assets/p5setup.js
+++ assets/p5setup.js
@@ -1,43 +1,45 @@
-var sounds = []
+var sounds = [];
 
 function setup() {
   // Override the loadImage method from p5js to enable the usage of relative paths
   // This method must be overriden inside of setup
-  let loadImageSuper = loadImage
+  let loadImageSuper = loadImage;
   loadImage = (path, successCallback, failureCallback) => {
-    if (!path.startsWith('file:') && !path.startsWith('http')) {
-      path = decodeURI(localPath) + path
+    if (!path.startsWith("file:") && !path.startsWith("http")) {
+      path = decodeURI(localPath) + path;
     }
-    return loadImageSuper.apply(this, [path, successCallback, failureCallback])
-  }
+    return loadImageSuper.apply(this, [path, successCallback, failureCallback]);
+  };
 
-  let loadSoundSuper = loadSound
+  let loadSoundSuper = loadSound;
   loadSound = (path, successCallback, errorCallback, whileLoadingCallback) => {
-    if (!path.startsWith('file:') && !path.startsWith('http')) {
-      path = decodeURI(localPath) + path
+    if (!path.startsWith("file:") && !path.startsWith("http")) {
+      path = decodeURI(localPath) + path;
     }
-    let sound = loadSoundSuper.apply(this, [path, successCallback, errorCallback, whileLoadingCallback])
+    let sound = loadSoundSuper.apply(this, [path, successCallback, errorCallback, whileLoadingCallback]);
     sounds.push(sound);
-    return sound
-  }
+    return sound;
+  };
 
-  var p5canvas = createCanvas(windowWidth-p5rulersize, windowHeight-p5rulersize)
-  p5canvas.parent('p5canvas')
-  frameRate(30)
-  clear()
+  var p5canvas = createCanvas(windowWidth - p5rulersize, windowHeight - p5rulersize);
+  p5canvas.parent("p5canvas");
+  frameRate(30);
+  clear();
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth-p5rulersize, windowHeight-p5rulersize)
-  clear()
+  resizeCanvas(windowWidth - p5rulersize, windowHeight - p5rulersize);
+  clear();
 }
 
 function p5reset() {
-  clear()
-  fill(255, 255, 255)
-  stroke(0, 0, 0)
-  strokeWeight(1)
-  textSize(12)
-  sounds.forEach((sound) => { sound.stop() })
+  clear();
+  fill(255, 255, 255);
+  stroke(0, 0, 0);
+  strokeWeight(1);
+  textSize(12);
+  sounds.forEach(sound => {
+    sound.stop();
+  });
   sounds = [];
 }
